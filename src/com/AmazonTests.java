f1011b940b30277fc3b826ca56f4dc93329d3e77
arunvincy <vincy.jaa@gmail.com>
2017-07-19 02:46:29 +0530

    First commit

---
+++ src/com/AmazonTests.java
@@ -0,0 +1,159 @@
+package com;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class AmazonTests {
+	 @Test
+		public void Page1Test() {
+	            
+	            Assert.assertEquals(true,true);
+		}
+	        
+	        @Test
+		public void Page2Test() {
+	            Reporter.log("M3 WAS CALLED");
+	            Assert.assertEquals(true,true);
+		}
+	        
+	        @Test
+		public void Page3Test() {
+	            Reporter.log("Page3Test WAS CALLED");
+	            String x = "10";
+	            x.equalsIgnoreCase(x);
+	            int y = Integer.parseInt(x);
+	            Assert.assertEquals(true,true);
+		}
+
+	        @Test
+		public void Page4Test() {
+	          
+	        	 Assert.assertEquals(true,true);
+	        	// throw new SkipException("Skipping - This is not ready for testing ");
+		}
+	        
+	        @Test
+			public void Page5Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        
+	        @Test
+			public void Page6Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        
+	        @Test
+			public void Page7Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        
+	        @Test
+			public void Page8Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        
+	        @Test
+			public void Page9Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        @Test
+			public void Page10Test() {
+		            
+		            Assert.assertEquals(true,true);
+			}
+	        
+	        @Test
+			public void Page11Test() { Assert.assertEquals(true,true);}
+		            
+	        @Test    public void Page11() { Assert.assertEquals(true,true);}
+	        @Test    public void Page12() { Assert.assertEquals(true,true);}
+	        @Test    public void Page13() { Assert.assertEquals(true,true);}
+	        @Test    public void Page14() { Assert.assertEquals(true,true);}
+	        @Test    public void Page15() { Assert.assertEquals(true,true);}
+	        @Test    public void Page16() { Assert.assertEquals(true,true);}
+	        @Test    public void Page17() { Assert.assertEquals(true,true);}
+	        @Test    public void Page18() { Assert.assertEquals(true,true);}
+	        @Test    public void Page19() { Assert.assertEquals(true,true);}
+	        @Test    public void Page20() { Assert.assertEquals(true,true);}
+	        @Test    public void Page21() { Assert.assertEquals(true,true);}
+	        @Test    public void Page22() { Assert.assertEquals(true,true);}
+	        @Test    public void Page23() { Assert.assertEquals(true,true);}
+	        @Test    public void Page24() { Assert.assertEquals(true,true);}
+	        @Test    public void Page25() { Assert.assertEquals(true,true);}
+	        @Test    public void Page26() { Assert.assertEquals(true,true);}
+	        @Test    public void Page27() { Assert.assertEquals(true,true);}
+	        @Test    public void Page28() { Assert.assertEquals(true,true);}
+	        @Test    public void Page29() { Assert.assertEquals(true,true);}
+	        @Test    public void Page30() { Assert.assertEquals(true,true);}
+	        @Test    public void Page31() { Assert.assertEquals(true,true);}
+	        @Test    public void Page32() { Assert.assertEquals(true,true);}
+	        @Test    public void Page33() { Assert.assertEquals(true,true);}
+	        @Test    public void Page34() { Assert.assertEquals(true,true);}
+	        @Test    public void Page35() { Assert.assertEquals(true,true);}
+	        @Test    public void Page36() { Assert.assertEquals(true,true);}
+	        @Test    public void Page37() { Assert.assertEquals(true,true);}
+	        @Test    public void Page38() { Assert.assertEquals(true,true);}
+	        @Test    public void Page39() { Assert.assertEquals(true,true);}
+	        @Test    public void Page40() { Assert.assertEquals(true,true);}
+	        @Test    public void Page41() { Assert.assertEquals(true,true);}
+	        @Test    public void Page42() { Assert.assertEquals(true,true);}
+	        @Test    public void Page43() { Assert.assertEquals(true,true);}
+	        @Test    public void Page44() { Assert.assertEquals(true,true);}
+	        @Test    public void Page45() { Assert.assertEquals(true,true);}
+	        @Test    public void Page46() { Assert.assertEquals(true,true);}
+	        @Test    public void Page47() { Assert.assertEquals(true,true);}
+	        @Test    public void Page48() { Assert.assertEquals(true,true);}
+	        @Test    public void Page49() { Assert.assertEquals(true,true);}
+	        @Test    public void Page50() { Assert.assertEquals(true,true);}
+	        @Test    public void Page51() { Assert.assertEquals(true,true);}
+	        @Test    public void Page52() { Assert.assertEquals(true,true);}
+	        @Test    public void Page53() { Assert.assertEquals(true,true);}
+	        @Test    public void Page54() { Assert.assertEquals(true,true);}
+	        @Test    public void Page55() { Assert.assertEquals(true,true);}
+	        @Test    public void Page56() { Assert.assertEquals(true,true);}
+	        @Test    public void Page57() { Assert.assertEquals(true,true);}
+	        @Test    public void Page58() { Assert.assertEquals(true,true);}
+	        @Test    public void Page59() { Assert.assertEquals(true,true);}
+	        @Test    public void Page60() { Assert.assertEquals(true,true);}
+	        @Test    public void Page61() { Assert.assertEquals(true,true);}
+	        @Test    public void Page62() { Assert.assertEquals(true,true);}
+	        @Test    public void Page63() { Assert.assertEquals(true,true);}
+	        @Test    public void Page64() { Assert.assertEquals(true,true);}
+	        @Test    public void Page65() { Assert.assertEquals(true,true);}
+	        @Test    public void Page66() { Assert.assertEquals(true,true);}
+	        @Test    public void Page67() { Assert.assertEquals(true,true);}
+	        @Test    public void Page68() { Assert.assertEquals(true,true);}
+	        @Test    public void Page69() { Assert.assertEquals(true,true);}
+	        @Test    public void Page70() { Assert.assertEquals(true,true);}
+	        @Test    public void Page71() { Assert.assertEquals(true,true);}
+	        @Test    public void Page72() { Assert.assertEquals(true,true);}
+	        @Test    public void Page73() { Assert.assertEquals(true,true);}
+	        @Test    public void Page74() { Assert.assertEquals(true,true);}
+	        @Test    public void Page75() { Assert.assertEquals(true,true);}
+	        @Test    public void Page76() { Assert.assertEquals(true,true);}
+	        @Test    public void Page77() { Assert.assertEquals(true,true);}
+	        @Test    public void Page78() { Assert.assertEquals(true,true);}
+	        @Test    public void Page79() { Assert.assertEquals(true,true);}
+	        @Test    public void Page80() { Assert.assertEquals(true,true);}
+	        @Test    public void Page81() { Assert.assertEquals(true,true);}
+	        @Test    public void Page82() { Assert.assertEquals(true,true);}
+	        @Test    public void Page83() { Assert.assertEquals(true,true);}
+	        @Test    public void Page84() { Assert.assertEquals(true,true);}
+	        @Test    public void Page85() { Assert.assertEquals(true,true);}
+	        @Test    public void Page86() { Assert.assertEquals(true,true);}
+	        @Test    public void Page87() { Assert.assertEquals(true,true);}
+	        @Test    public void Page88() { Assert.assertEquals(true,true);}
+	        @Test    public void Page89() { Assert.assertEquals(false,true);}
+	        @Test    public void Page90() { Assert.assertEquals(false,true);}
+	        @Test    public void Page91() { Assert.assertEquals(false,true);}
+      
+			
+	}
+
